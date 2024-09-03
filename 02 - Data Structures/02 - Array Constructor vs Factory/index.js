44a8870aab5cd460b707f30a3cfa99c25d988229
mdmahfuz307 <njnirob2019@gmail.com>
2024-09-03 08:56:11 +0600

    Array Construvtor & Array Factory Pattern

--- 02 - Data Structures/02 - Array Constructor vs Factory/index.js
+++ 02 - Data Structures/02 - Array Constructor vs Factory/index.js
@@ -2,10 +2,24 @@
 
 
 // Constructor Pattern
-const a1 = new Array(); // []
-console.log(a1,a1.length);
+const a = new Array(); // []
+console.log(a,a.length);
 
+const b = new Array(5);
+console.log(b,b.length);
+
+const c = new Array(1,2,3,4,5);
+console.log.apply(c,c.length);
 
 
 
 // Factory Pattern
+
+const a1 = Array(); // []
+console.log(a1,a1.length);
+
+const a2 = Array(5);
+console.log(a2,a2.length);
+
+const a3 = Array(1,2,3,4,5);
+console.log.apply(a3,a3.length);
