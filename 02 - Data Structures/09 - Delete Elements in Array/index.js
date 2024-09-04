6f9a2657a8614ad7a3b5a1454e38643bac66f058
mdmahfuz307 <njnirob2019@gmail.com>
2024-09-04 10:22:20 +0600

    total 2ta array aca ai 2tar valu gula 1 jaigai nye ekta array banabo

--- 02 - Data Structures/09 - Delete Elements in Array/index.js
+++ 02 - Data Structures/09 - Delete Elements in Array/index.js
@@ -1,38 +1,5 @@
-let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+const arr = [1,2,3];
+const arr1 = [4,5,6];
 
-// Remove First Element
-numbers.shift();
-
-// Remove Last Element
-numbers.pop();  //numbers.length = numbers.length-1; (Chaile ai vbe o krte pari)
-
-
-// Remove Inside Elements - Splice using Index
-numbers.splice(2,1);
-
-
-// Remove Inside Elements - Splice using Value
-const toBeDeleted = 5;
-for(let i =0;i<numbers.length;i++)
-{
-    if(numbers[i] == toBeDeleted)
-    {
-        numbers.splice(i,1);
-    }
-}
-
-
-// Filter Array
-const toBeDeleted1 = 6;
-numbers = numbers.filter((item) => item != toBeDeleted1);
-
-
-// Reset whole array
-let xx = [1,2,3];
-let yy = xx;
-xx.length = 0;
-//console.log(xx, yy);
-
-
-// Reset array using while loop
-while(numbers.length) numbers.pop();
+const arr2 = arr.concat(arr1);
+console.log(arr2);
---
+++ 02 - Data Structures/10 - Concat Multiple Arrays/index.js
@@ -0,0 +1,10 @@
+// total 2ta array aca ai 2tar valu gula 1 jaigai nye ekta array banabo
+
+const arr = [1,2,3];
+const arr1 = [4,5,6];
+
+const arr2 = arr.concat(arr1);
+
+// ORno vabe o krte Pari
+const arr3 = [...arr,...arr1];
+
