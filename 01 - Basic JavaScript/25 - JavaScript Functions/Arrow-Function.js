09152290cb958657283eb6da42d5b8b857c36b0f
mdmahfuz307 <njnirob2019@gmail.com>
2024-09-05 18:16:42 +0600

    Arrow-Function

---
+++ 01 - Basic JavaScript/25 - JavaScript Functions/Arrow-Function.js
@@ -0,0 +1,21 @@
+// Arrow Function
+const printThis = () => {
+    console.log('Printing');
+}
+printThis();
+
+
+
+// Arrow Function with Parameter
+const addThis = (a,b) => {
+    return a * b;
+}
+addThis(234,2232);
+
+
+
+// Sort Syntex
+const print = (a) => console.log(a);
+
+const returnSomething = (a) => 2 * a;
+returnSomething(34);
