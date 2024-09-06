229bccf95c8e1e1942fbc95b58a07f848b3620fb
mdmahfuz307 <njnirob2019@gmail.com>
2024-09-06 18:28:53 +0600

    Spread Operator(...arr)

--- 01 - Basic JavaScript/27 - Spread Operator/index.js
+++ 01 - Basic JavaScript/27 - Spread Operator/index.js
@@ -1,2 +1,26 @@
 // Spread Operator
 
+let arr = [1,2,3,4,5,6,7];
+
+let arr2 = [...arr,8,9,10,11];
+
+console.log(arr2);   // OutPut :- [1,2,3,4,5,6,7,8,9,10,11]
+
+
+
+
+// Sum of Spread Operator
+let ar = [5,6,7,8];
+
+function sum(num1,num2,num3,num4){
+    return num1 + num2 + num3 + num4;
+}
+
+console.log(sum(...ar));  // OutPut :- 26
+
+
+
+// Max & MIn in Spread
+let array = [6,7,4,3,2,8,9];
+
+console.log(Math.max(...array));
