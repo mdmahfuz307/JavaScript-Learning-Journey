fc05b507440043556a29072a38f7e5eab5f6451b
mdmahfuz307 <njnirob2019@gmail.com>
2024-09-12 20:03:39 +0600

    Date

---
+++ 01 - Basic JavaScript/29 - Date Object/index.js
@@ -0,0 +1,32 @@
+// Normally Date ber korte use krbo
+let myDates = new Date();
+
+console.log(myDates);
+
+
+
+// String Akar e ber korar jrno ai vbe use krbo
+let myDate = new Date();
+
+console.log(myDate.toDateString());
+
+
+
+// Date / Month / Year alada alada dekhar jrno
+let mDate = new Date();
+
+console.log(mDate.getDay());
+
+console.log(mDate.getDate());
+
+console.log(mDate.getFullYear());
+
+console.log(mDate.getHours());
+
+console.log(mDate.getMilliseconds());
+
+console.log(mDate.getMinutes());
+
+console.log(mDate.getMonth());
+
+console.log(mDate.getSeconds());
