4a451188c73a128d3c7d652828511bbd47d080f7
mdmahfuz307 <njnirob2019@gmail.com>
2024-09-12 20:27:59 +0600

    Number Method

--- 01 - Basic JavaScript/05 - number method/script.js
+++ 01 - Basic JavaScript/05 - number method/script.js
@@ -1,27 +1,27 @@
-// toString method
+// toString method it returns thr given number in the from string.
 var num1 = 10;
 num1 = toString(num1);
 console.log(typeof (num1));
 
-// parseFloat method
+// parseFloat method it converts the given string into a floating point number.
 var num2 = "10.234";
 num2 = parseFloat(num2);
 console.log(num2);
 console.log(typeof (num2));
 
 
-// parseInt method
+// parseInt method it converts the given string into an integer number.
 var value = "12";
 value = parseInt(value);
 console.log(typeof (value));
 
 
-// toFixed method
+// toFixed method it returns the string that represents a number with exact digits after a decimal point.
 var num3 = 124.456;
 newNumber = num3.toFixed(1);
 console.log(newNumber);
 
-// otoPrecision method
+// toPrecision method
 var num4 = 124.456;
 console.log(num4.toPrecision(6))
 
