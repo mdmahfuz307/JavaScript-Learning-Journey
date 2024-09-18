92fec67caf55de464262594acf46ad252d6bd9b7
mdmahfuz307 <njnirob2019@gmail.com>
2024-09-18 10:25:22 +0600

    DOM traversing & manipulating

---
+++ 03 - JS HTML DOM/05 - DOM traversing & manipulating/script.js
@@ -0,0 +1,22 @@
+/*
+
+Downwards
+---------
+--> querySelector() / querySelectorAll()
+--> children
+
+Upwards
+-------
+--> parentELement
+
+Sideways
+--------
+--> nextElementSibling,
+    previousElementSibling
+--> parentElement + children + index
+
+*/
+
+let myDiv = document.getElementById("div-id");
+let firstChild = myDiv.firstElementChild; // get the first child element
+firstChild.style.color = 'red'; // change text color to re
